c4d7f301f82dcddd9a1dcc2fa27b11ffb53310de
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-07-26 20:14:28 +0500

    chap 3 complete and done 4th at the start

--- vueproj/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
+++ vueproj/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
@@ -4,21 +4,36 @@
       <div class="navbar-item">
         <div class="buttons">
           <a
-            href="https://github.com/johnpapa/vue-getting-started"
+            :href="github"
             target="_blank"
             rel="noopener noreferrer"
           >
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
           </a>
           <a
-            href="https://twitter.com/john_papa"
+            :href="twitter"
             target="_blank"
             rel="noopener noreferrer"
           >
             <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+          <a :href="facebook" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook fa-2x" aria-hidden="true"></i>
           </a>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  name:"HeaderBarLinks",
+data(){
+  return{
+          github:'https://github.com/abdullah4a/vueproject',
+          twitter:'https://twitter.com/account/access',
+          facebook:'https://facebook.com/abdullah4a'
+        }
+      } 
+      }
+</script>
--- vueproj/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ vueproj/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -13,41 +13,69 @@
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly></label>
+                <label class="input" id="id" readonly>{{ Hero.id }}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="Hero.firstName" />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="Hero.LastName" />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" />
+                <textarea
+                  class="input"
+                  id="description"
+                  type="text"
+                  v-model="Hero.description"
+                />
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
+                  <input
+                    type="radio"
+                    id="color-red"
+                    value="red"
+                    v-model="Hero.capecolor"
+                  />
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
+                  <input
+                    type="radio"
+                    id="color-blue"
+                    value="blue"
+                    v-model="Hero.capecolor"
+                  />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
+                  <input
+                    type="radio"
+                    id="color-green"
+                    value="green"
+                    v-model="Hero.capecolor"
+                  />
                   green
                 </label>
-                <div class="color-line"></div>
+                <div
+                  class="color-line"
+                  :style="{ 'background-color': Hero.capecolor }"
+                ></div>
               </div>
               <div class="field">
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power">
+                    <select
+                      id="power"
+                      v-model="Hero.power"
+                      @keyup.esc="clearPower"
+                      :class="{ invalid: !Hero.power }"
+                    >
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -60,23 +88,33 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input
+                    type="checkbox"
+                    class="is-primary"
+                    id="active"
+                    v-model="Hero.active"
+                  />
                 </label>
               </div>
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button
+              class="link card-footer-item cancel-button"
+              @click="cancelHero"
+            >
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item" @click="saveHero()">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info"></div>
+        <div class="notification is-info">
+          <pre>{{ message }}</pre>
+        </div>
       </div>
     </div>
   </div>
@@ -85,5 +123,35 @@
 <script>
 export default {
   name: 'Heroes',
+  data() {
+    return {
+      Hero: {
+        id: '010',
+        firstName: 'Super Man',
+        LastName: 'Bat Man',
+        description: 'The power to Fly',
+        capecolor: '',
+        active: 'false',
+        power: '',
+      },
+      message: '',
+    };
+  },
+  methods: {
+    cancelHero() {
+      this.message = '';
+    },
+    saveHero() {
+      this.message = JSON.stringify(this.Hero, null, '\n');
+    },
+    clearPower() {
+      this.Hero.power = '';
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+#power {
+  margin: 3px;
+}
+</style>
