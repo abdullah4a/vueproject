c896f89f6f7f1d7da2335c355e5e10b0eef93db0
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-07-27 11:55:12 +0500

    heroes.vue updated

--- vueproj/04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ vueproj/04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -8,10 +8,20 @@
         <header class="card-header">
           <p class="card-header-title">heroes list</p>
         </header>
-        <ul class="list is-hoverable"></ul>
+        <ul class="list is-hoverable">
+          <li v-for="hero in heroes" :key="hero.id">
+            <a
+              class="list-item"
+              @click="selectedHero = hero"
+              :class="{ 'is-active': selectedHero === hero }"
+            >
+              <span>{{ hero.firstName }}</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-    <div class="columns">
+    <div class="columns" v-if="selectedHero">
       <div class="column is-3">
         <header class="card-header">
           <p class="card-header-title">
@@ -62,12 +72,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {
-        id: 111,
-        firstName: '...',
-        lastName: '...',
-        description: '...',
-      },
+      selectedHero: undefined,
       heroes: [
         {
           id: 10,
