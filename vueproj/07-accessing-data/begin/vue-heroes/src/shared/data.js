c890a26c9f43f49824b280352e54e08a105043c3
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-01 22:59:06 +0500

    Chapter 7 Completed

--- vueproj/07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ vueproj/07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -1,16 +1,30 @@
 import * as axios from 'axios';
-import { format }
-from 'date-fns';
+import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
 import { API } from './config';
 const getHeroes = async function() {
-    const responce = await axios.get(`${API}/heroes.json`);
-    const heroes = responce.data.map(h => {
-        h.originDate = format(h.originDate, inputDateFormat);
-        return h;
-    });
-    return heroes;
+    try {
+        const responce = await axios.get(`${API}/heroes.json`);
+        let data = parselist(responce);
+        const heroes = data.map(h => {
+            h.originDate = format(h.originDate, inputDateFormat);
+            return h;
+        });
+        return heroes;
+    } catch (error) {
+        console.error(`There is an Error ${error}`);
+        return [];
+    }
 }
+const parselist = response => {
+    if (response.status != 200) throw Error(response.message);
+    if (!response.data) return [];
+    let list = response.data;
+    if (typeof list != "object") {
+        list = [];
+    };
+    return list;
+};
 
 export const data = {
     getHeroes,
