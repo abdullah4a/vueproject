77f819540a1dc17438fd4975e360a686a3affc96
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-25 11:01:23 +0500

    Update logging-mixins.js

--- vueproj/07-accessing-data/begin/vue-heroes/src/shared/logging-mixins.js
+++ vueproj/07-accessing-data/begin/vue-heroes/src/shared/logging-mixins.js
@@ -3,38 +3,38 @@ import { logger } from './';
 const hookMessageSuffix = 'hook called (from mixin)';
 
 export const lifecycleHooks = {
-  // Computeds
-  computed: {
-    componentName() {
-      return `${this.$options.name} component`;
+    // Computeds
+    computed: {
+        componentName() {
+            return `${this.$options.name} component`;
+        },
     },
-  },
-  // LifeCycle Hooks
-  created() {
-    logger.info(`${this.componentName} created ${hookMessageSuffix}`);
-    logger.info('component data', this.$data);
-  },
-  mounted() {
-    logger.info(`${this.componentName} mounted ${hookMessageSuffix}`);
-  },
-  updated() {
-    logger.info(`${this.componentName} updated ${hookMessageSuffix}`);
-  },
-  destroyed() {
-    logger.info(`${this.componentName} destroyed ${hookMessageSuffix}`);
-  },
+    // LifeCycle Hooks
+    created() {
+        logger.info(`${this.componentName} created ${hookMessageSuffix}`);
+        logger.info('component data', this.$data);
+    },
+    mounted() {
+        logger.info(`${this.componentName} mounted ${hookMessageSuffix}`);
+    },
+    updated() {
+        logger.info(`${this.componentName} updated ${hookMessageSuffix}`);
+    },
+    destroyed() {
+        logger.info(`${this.componentName} destroyed ${hookMessageSuffix}`);
+    },
 };
 
 export const heroWatchers = {
-  // Watchers
-  watch: {
-    selectedHero: {
-      immediate: true,
-      deep: true,
-      handler(newValue, oldValue) {
-        logger.info('old values', oldValue);
-        logger.info('new values', newValue);
-      },
+    // Watchers
+    watch: {
+        selectedHero: {
+            immediate: true,
+            deep: true,
+            handler(newValue, oldValue) {
+                logger.info('old values', oldValue);
+                logger.info('new values', newValue);
+            },
+        },
     },
-  },
 };
